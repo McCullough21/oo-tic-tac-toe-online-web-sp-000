054e74667dabcec4f3bf4cc79a4247ca17cd9373
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-02 18:31:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,9 +3,8 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
 
 attr_accessor :board
 
-def initialize(board)
-  new_game = self.new
-  @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def initialize(board = nil)
+  @board = board || Array.new(9, " ")
 end
 
 end
