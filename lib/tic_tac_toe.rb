8fb99ccbfee9281af949eec3e37733b8967d1331
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-02 18:27:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,11 @@
 class TicTacToe
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
+attr_accessor :board
+
 def initialize(board)
   board = [" "," ", " ", " ", " ", " ", " ", " ", " "]
   @board = board
+end
+
 end
