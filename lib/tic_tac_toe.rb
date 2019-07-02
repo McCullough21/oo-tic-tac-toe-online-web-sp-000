08068d3b8caa6042318c5fab861b986fe5637bd2
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-02 18:47:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,7 +21,7 @@ def move(index, player)
   @board[index] = player
 end
 def position_taken?(board, index)
-   board[index] == "O" || board[index] == "X"  
+   board[index] == "X" || board[index] == "O"  
 end
 
 end
